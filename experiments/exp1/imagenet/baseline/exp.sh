f0a7d450fe1e92e96b3fdebdbc4aff03755b5099
Ubuntu <ubuntu@ip-172-31-87-22.ec2.internal>
2023-01-19 05:23:34 +0000

    update

--- experiments/exp1/imagenet/baseline/exp.sh
+++ experiments/exp1/imagenet/baseline/exp.sh
@@ -2,19 +2,23 @@
 
 # According to the benchmark results in https://lambdalabs.com/gpu-benchmarks,
 # the training time of ResNet-50 range from 135 to 1197 images/s.
-# Therefore, we set the compute time as 0.4s - 4.0s with batch size 512.
-compute_time=( 0.4 1.2 2.0 2.8 3.6 4.0 )
-pod="ilsvrc-baseline"
-node="129.59.234.237"
 
-for((i=0;i<5;i++))
+repeat=1
+compute_time=( 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 )
+batch_size=( 256 512 1024 2048 )
+pod="ilsvrc-baseline"
+
+for((i=0;i<$repeat;i++))
 do
     for t in ${compute_time[*]}
     do
-        mkdir -p data/run$i/$t
-        kubectl exec $pod -- vmtouch -e /nfs_storage/
-        sudo ssh -i ~/.ssh/id_rsa ubuntu@$node vmtouch -e /nfs_storage/
-        kubectl exec $pod -- python3 main.py -b 512 --sim-compute-time $t --epochs 1 --mini-batches 50
-        kubectl cp $pod:/app/load_time.npy data/run$i/$t/load_time.npy
+        for b in ${batch_size[*]}
+        do
+            data_dir=data/run$i/$t/$b
+            mkdir -p $data_dir
+            kubectl exec $pod -- vmtouch -e /nfs_storage/
+            kubectl exec $pod -- python3 main.py -b $b --sim-compute-time $t --epochs 1 --mini-batches 100
+            kubectl cp $pod:/app/load_time.npy $data_dir/load_time.npy
+        done
     done
 done
--- experiments/exp1/imagenet/dlcache/exp.sh
+++ experiments/exp1/imagenet/dlcache/exp.sh
@@ -2,25 +2,31 @@
 
 # According to the benchmark results in https://lambdalabs.com/gpu-benchmarks,
 # the training time of ResNet-50 range from 135 to 1197 images/s.
-# Therefore, we set the compute time as 0.4s - 4.0s with batch size 512.
-compute_time=( 0.4 1.2 2.0 2.8 3.6 4.0 )
-node="129.59.234.237"
-pod="ilsvrc27"
 
-for((i=4;i<6;i++))
+repeat=1
+compute_time=( 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 )
+batch_size=( 256 512 1024 2048 )
+pod="ilsvrc"
+node="ip-172-31-92-64"
+
+for((i=0;i<$repeat;i++))
 do
     for t in ${compute_time[*]}
     do
-        mkdir -p data/run$i/$t
-        kubectl exec $pod --container client -- vmtouch -e /$node/
-        sudo ssh -i ~/.ssh/id_rsa ubuntu@$node vmtouch -e /nfs_storage
-        kubectl exec $pod -- python3 main.py -b 512 --sim-compute-time $t --epochs 1 --mini-batches 50
-        kubectl cp $pod:/app/exp.npy data/run$i/$t/load_time.npy --container job
-        for((k=0;k<4;k++))
+        for b in ${batch_size[*]}
         do
-            kubectl cp $pod:/app/cache_hits_$k.npy data/run$i/$t/cache_hits_$k.npy --container job
+            data_dir=data/run$i/$t/$b
+            mkdir -p $data_dir
+            kubectl exec $pod --container client -- vmtouch -e /$node/
+            kubectl exec $pod -- python3 main.py -b 512 --sim-compute-time $t --epochs 1 --mini-batches 50
+            kubectl cp $pod:/app/load_time.npy $data_dir/load_time.npy --container job
+            for((k=0;k<4;k++))
+            do
+                kubectl cp $pod:/app/cache_hits_$k.npy $data_dir/cache_hits_$k.npy --container job
+            done
+            kubectl cp $pod:/train_cache_usage.npy $data_dir/train_cache_usage.npy --container client
+            kubectl exec $pod pkill python3
+            sleep 3
         done
-        kubectl cp $pod:/share/train_cache_usage.npy data/run$i/$t/train_cache_usage.npy --container job
-        kubectl exec $pod pkill python3
     done
 done
