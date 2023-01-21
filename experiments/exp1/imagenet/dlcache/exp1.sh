9c0375c44eddc9461c52f8fa712a8e5f86253a4a
Ubuntu <ubuntu@ip-172-31-87-22.ec2.internal>
2023-01-21 01:10:53 +0000

    udpate

--- experiments/exp1/imagenet/dlcache/exp1.sh
+++ experiments/exp1/imagenet/dlcache/exp1.sh
@@ -3,13 +3,13 @@
 # According to the benchmark results in https://lambdalabs.com/gpu-benchmarks,
 # the training time of ResNet-50 range from 135 to 1197 images/s.
 
-repeat=1
+repeat=3
 # compute_time=( 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 )
 compute_time=( 0.4 0.8 1.2 1.6 2.0 2.4 )
 batch_size=( 256 512 1024 2048 )
-node="172.31.92.64"
+node="172.31.90.206"
 
-for((i=0;i<$repeat;i++))
+for((i=2;i<$repeat;i++))
 do
     for t in ${compute_time[*]}
     do
