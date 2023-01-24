7b07e168b0204278a0aeb56cab1bb9654310e136
Ubuntu <ubuntu@ip-172-31-87-22.ec2.internal>
2023-01-24 08:28:51 +0000

    update

--- experiments/exp1/imagenet/dlcache/exp.sh
+++ experiments/exp1/imagenet/dlcache/exp.sh
@@ -3,24 +3,21 @@
 # According to the benchmark results in https://lambdalabs.com/gpu-benchmarks,
 # the training time of ResNet-50 range from 135 to 1197 images/s.
 
-repeat=3
-# compute_time=( 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 )
+i=0
+w=8
 compute_time=( 0.4 0.8 1.2 1.6 2.0 2.4 )
 batch_size=( 256 512 1024 2048 )
-node="172.31.90.206"
+node="172.31.92.64"
 
-for((i=2;i<$repeat;i++))
+for t in ${compute_time[*]}
 do
-    for t in ${compute_time[*]}
+    for b in ${batch_size[*]}
     do
-        for b in ${batch_size[*]}
-        do
-            data_dir=data/run$i/$t/$b
-            mkdir -p $data_dir
-            vmtouch -e /$node/
-            python3 main.py -b $b --sim-compute-time $t --epochs 1 --mini-batches 100
-            mv *.npy $data_dir/
-            mv /share/train_cache_usage.npy $data_dir/
-        done
+        data_dir=data/run$i/$t/$b
+        mkdir -p $data_dir
+        vmtouch -e /$node/
+        python3 main.py -b $b --sim-compute-time $t --epochs 1 --mini-batches 100 -j $w
+        mv *.npy $data_dir/
+        mv /share/train_cache_usage.npy $data_dir/
     done
 done
