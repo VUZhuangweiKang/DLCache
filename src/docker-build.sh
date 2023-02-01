00195dca225f3e4be07b823ed63ac7072bfd41d1
Ubuntu <ubuntu@ip-172-31-87-22.ec2.internal>
2023-02-01 03:14:56 +0000

    update

--- src/docker-build.sh
+++ src/docker-build.sh
@@ -14,10 +14,21 @@ then
     docker build -t dlcache-dev:manager-worker -f manager-worker/Dockerfile .
     docker tag dlcache-dev:manager-worker zhuangweikang/dlcache-dev:manager-worker
     docker push zhuangweikang/dlcache-dev:manager-worker
-else
+elif [ $1 == "client" ]
+then
     docker rmi -f dlcache-dev:client
     docker rmi -f zhuangweikang/dlcache-dev:client
     docker build -t dlcache-dev:client -f client/Dockerfile .
     docker tag dlcache-dev:client zhuangweikang/dlcache-dev:client
-    docker push zhuangweikang/dlcache-dev:client  
+    docker push zhuangweikang/dlcache-dev:client
+elif [ $1 == "deepspeech" ]
+then
+    docker rmi zhuangweikang/deepspeech-dev:latest
+    docker build -t zhuangweikang/deepspeech-dev:latest -f datasets/LibriSpeech/Dockerfile .
+    docker push zhuangweikang/deepspeech-dev:latest
+elif [ $1 == "imagenet" ]
+then
+    docker rmi zhuangweikang/imagedatasets-dev:latest
+    docker build -t zhuangweikang/imagedatasets-dev:latest -f datasets/ImageNet/Dockerfile .
+    docker push zhuangweikang/imagedatasets-dev:latest
 fi
